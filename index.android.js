a58ad9b3fe8a033c29b4c748724c55c1f947158a
chdonncha <chdonncha@gmail.com>
2016-04-15 13:54:43 +0100

    Working ListView

--- index.android.js
+++ index.android.js
@@ -2,50 +2,72 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
+ 'use strict';
+ var React = require('react-native');
 
-import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const { AppRegistry, StyleSheet, Text, View, ListView, AlertIOS } = React;
 
-class GroceryApp extends Component {
+const Firebase = require('firebase');
+const styles = require('./styles.js')
+
+
+
+class GroceryApp extends React.Component {
+  _renderItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}} />
+    );
+  }
+
+  constructor(props) {
+  super(props);
+  this.itemsRef = new Firebase("https://<YOURFIREBASE>.firebaseio.com");
+
+  this.state = {
+    dataSource: new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    })
+  };
+}
+
+listenForItems(itemsRef) {
+  itemsRef.on('value', (snap) => {
+    // get children as an array
+    var items = [];
+    snap.forEach((child) => {
+      items.push({
+        title: child.val().title,
+        _key: child.key()
+      });
+    });
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows(items)
+    });
+  });
+}
+
+componentDidMount() {
+  //this.setState({
+    //dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+  //})
+  this.listenForItems(this.itemsRef);
+}
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <StatusBar title="Grocery List" />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          style={styles.listview}/>
+        <ActionButton title="Add" onPress={() => {}} />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('GroceryApp', () => GroceryApp);
